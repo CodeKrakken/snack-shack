d8a4ba19c1b9afdb27ede6def3180a82a0342b67
CodeKrakken <newholm1@hotmail.com>
2021-02-03 18:05:34 +0000

    Add array for sandwich orders

--- spec/SnackShackSpec.js
+++ spec/SnackShackSpec.js
@@ -1,2 +1,14 @@
 'use strict';
 const SnackShack = require('../lib/SnackShack.js')
+
+describe('SnackShack', function() {
+  let snackshack
+
+  beforeEach(function() {
+    snackshack = new SnackShack()
+  })
+
+  it('has an array for sandwich orders', function() {
+    expect(snackshack.orders).toBeDefined()
+  })
+})
