90f59bb0fce93c3cd98a8eea19c599cc7ae3f947
CodeKrakken <newholm1@hotmail.com>
2021-02-05 12:51:24 +0000

    Add Take a Well Earned Break to schedule

--- spec/SnackShackSpec.js
+++ spec/SnackShackSpec.js
@@ -17,13 +17,19 @@ describe('SnackShack', function() {
   })
 
   it('returns preparation time', function() {
-    expect(snackshack.buy(1)).toEqual("0:00 1 sandwich order placed, start making sandwich 1\n1:00 serve sandwich 1")
-    expect(snackshack.buy(2)).toEqual("0:00 2 sandwich orders placed, start making sandwich 1\n1:00 serve sandwich 1\n1:30 start making sandwich 2\n2:00 serve sandwich 2")
-    // expect(snackshack.buy(6000)).toEqual('9000:00')
+    expect(snackshack.buy(1)).toEqual("0:00 1 sandwich order placed, start making sandwich 1\n"
+                                    + "1:00 serve sandwich 1\n"
+                                    + "1:30 take a well earned break!")
+    expect(snackshack.buy(2)).toEqual("0:00 2 sandwich orders placed, start making sandwich 1\n"
+                                    + "1:00 serve sandwich 1\n"
+                                    + "1:30 start making sandwich 2\n"
+                                    + "2:30 serve sandwich 2")
   })
 
   it('assumes an order of 1 unless specified', function() {
-    expect(snackshack.buy()).toEqual("0:00 1 sandwich order placed, start making sandwich 1\n1:00 serve sandwich 1")
+    expect(snackshack.buy()).toEqual("0:00 1 sandwich order placed, start making sandwich 1\n"
+                                   + "1:00 serve sandwich 1\n"
+                                   + "1:30 take a well earned break!")
   })
 
   it('will only accept a positive integer order', function() {
