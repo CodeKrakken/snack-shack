9acae5ba8aa3358f720572d8ee62b7b905540121
CodeKrakken <newholm1@hotmail.com>
2021-02-05 11:59:44 +0000

    Return schedule as requested

--- spec/SnackShackSpec.js
+++ spec/SnackShackSpec.js
@@ -17,13 +17,13 @@ describe('SnackShack', function() {
   })
 
   it('returns preparation time', function() {
-    expect(snackshack.buy(1)).toEqual("1:30")
-    expect(snackshack.buy(2)).toEqual("3:00")
-    expect(snackshack.buy(6000)).toEqual('9000:00')
+    expect(snackshack.buy(1)).toEqual("0:00 1 sandwich orders placed, start making sandwich 1\n1:00 serve sandwich 1")
+    // expect(snackshack.buy(2)).toEqual("0:00 2 sandwich orders placed, start making sandwich 1\n0:60 serve sandwich 1\n1:")
+    // expect(snackshack.buy(6000)).toEqual('9000:00')
   })
 
   it('assumes an order of 1 unless specified', function() {
-    expect(snackshack.buy()).toEqual("1:30")
+    expect(snackshack.buy()).toEqual("0:00 1 sandwich orders placed, start making sandwich 1\n1:00 serve sandwich 1")
   })
 
   it('will only accept a positive integer order', function() {
